0b51e34fbbd96b0b218399462242d76e0b4a3daf
Scott Semple <scott@scottsemple.com>
2013-06-12 21:17:57 -0400

    Nuf fer now

---
+++ ch6.rb
@@ -0,0 +1,10 @@
+# CHAPTER 6
+
+## Angry boss
+puts "What do you want?"
+want = gets.chomp
+puts "Whaddaya mean #{want}? You're fired!"
+
+## Table of contents
+line_width = 50
+puts "Table of Contents"
