6b174c3e4786bfd3b7a41d7717b1a62782bfb5c3
Scott Semple <scott@scottsemple.com>
2013-06-12 20:50:26 -0400

    Test this set up

---
+++ lessons.rb
@@ -0,0 +1,14 @@
+# CHAPTER 2
+# Hours in a year
+puts 24 * 365
+
+# Minutes in a decade
+puts 60 * 24 * 365 * 10
+
+# My age in seconds
+puts 39 * 365 * 24 * 60 * 60
+
+# Chris Pine's age
+puts 1111000000 / 60 / 60 / 24 / 365
+
+# CHAPTER 5
