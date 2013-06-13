0f621f00b938984df0915dbd2eef3afe8eaa07cd
Scott Semple <scott@scottsemple.com>
2013-06-12 21:12:36 -0400

    Change file names to chpater names

--- ch2.rb
+++ ch2.rb
@@ -1,5 +1,6 @@
 # CHAPTER 2
-# Hours in a year
+
+## Hours in a year
 puts 24 * 365
 
 # Minutes in a decade
@@ -10,5 +11,3 @@ puts 39 * 365 * 24 * 60 * 60
 
 # Chris Pine's age
 puts 1111000000 / 60 / 60 / 24 / 365
-
-# CHAPTER 5
