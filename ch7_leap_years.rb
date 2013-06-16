8d8372a8589a0ecbc5c826699678a19db76807dc
Scott Semple <scott@scottsemple.com>
2013-06-15 21:21:10 -0600

    Push final version of leap year exercise

--- ch7_leap_years.rb
+++ ch7_leap_years.rb
@@ -2,47 +2,32 @@
 
 # Leap Year calculator
 
-puts "Starting year?"
+puts "Let's find all of the leap years within a given range."
+puts "What year would you like to start at?"
 start_year = gets.chomp.to_i
-puts "End year?"
+puts "What year would you like to end at?"
 end_year = gets.chomp.to_i
-puts "These are leap years within that range:"
-
- /
-start: 1965
-end: 1987
-
-Need to get:
-1968
-1972
-1976
-1980
-1984
-
-((1965%4) * 4) + 4 = 1968
-/
+puts "These are the leap years within that range:"
 
 year = start_year
 
 if year % 4 == 0
-  # if no remainder, puts start and add four until end_year
-  puts year
-  while true
-    year += 4
-    puts year
-    if year >= end_year
-    	break
-    end
+  if year % 100 == 0
+  	year += 4
   end
+  puts year
 else
-  # if remainder, then modulus*4 plus 4, add four until end_year
   year = (start_year / 4 * 4) + 4
   puts year
-  while true
-    year += 4
-    puts year
-    if year >= end_year
-    	break
-    end
+end
+
+while true
+  year += 4
+  if year >= end_year
+  	break
   end
+  if year % 100 == 0
+  	year += 4
+  end
+  puts year
 end
