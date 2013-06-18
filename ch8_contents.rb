c9683f05cbe8d9a7afd02f6d650fa0065f83e15b
Scott Semple <scott@scottsemple.com>
2013-06-18 07:04:31 -0600

    Update contents exercise, remove others

--- ch8_contents.rb
+++ ch8_contents.rb
@@ -2,11 +2,14 @@
 
 ## Table of contents, revisited
 
-toc = ["Table of Contents", "Chapter 1", "page 1", "Chapter 2", "page 9", "Chapter 3", "page 13"]
-
+title = "Table of Contents"
+chapters = ["Chapter 1", "Chapter 2", "Chapter 3"]
+pages = ["1", "9", "13"]
 line_width = 26
-puts (toc[0].center(line_width * 2))
+
 puts ""
-puts (toc[1].ljust(line_width) + toc[2].rjust(line_width))
-puts ("Chapter 2: Numbers".ljust(line_width) + "page 9".rjust(line_width))
-puts ("Chapter 3: Letters".ljust(line_width) + "page 13".rjust(line_width))
+puts (title.center(line_width * 2))
+puts ""
+chapters.each do |chap| & pages.each do |page|
+	puts (chap.ljust(line_width) + "page " page.rjust(line_width))
+end
