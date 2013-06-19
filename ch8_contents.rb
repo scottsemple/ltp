486459940bea43db9189e27ecb3081aed215abad
Scott Semple <scott@scottsemple.com>
2013-06-18 18:57:10 -0600

    Romans and such

--- ch8_contents.rb
+++ ch8_contents.rb
@@ -3,13 +3,14 @@
 ## Table of contents, revisited
 
 title = "Table of Contents"
-chapters = ["Chapter 1", "Chapter 2", "Chapter 3"]
-pages = ["1", "9", "13"]
+chapters = [["1: Getting Started", "page 1"], ["2: Numbers", "page 9"], ["3: Letters", "page 13"]]
 line_width = 26
 
 puts ""
 puts (title.center(line_width * 2))
 puts ""
-chapters.each do |chap| & pages.each do |page|
-	puts (chap.ljust(line_width) + "page " page.rjust(line_width))
+chapters.each do |chap|
+	name = chap[0]
+	page = chap[1]
+	puts ("Chapter " + name.ljust(line_width) + page.rjust(line_width))
 end
