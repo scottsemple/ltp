0548c1db492c3eb32a5ec9241a1fbb4053f5761d
Scott Semple <scott@scottsemple.com>
2013-06-20 20:59:32 -0600

    Initial continent exercise with notes

--- ch10_continents.rb
+++ ch10_continents.rb
@@ -2,7 +2,6 @@
 
 ## Calculating the size of the continent
 
-# These are just to make the map easier for me to read. # "M" is visually more dense than "o".
 M = 'land'
 o = 'water'
 
@@ -10,7 +9,7 @@ world = [[o,o,o,o,o,o,o,o,o,o,o],
          [o,o,o,o,M,M,o,o,o,o,o],
          [o,o,o,o,o,o,o,o,M,M,o],
          [o,o,o,M,o,o,o,o,o,M,o],
-         [o,o,o,M,o,M,M,o,o,o,o],
+         [o,o,o,M,o,M,M,o,o,o,o], # But 5,5 is water, no?
          [o,o,o,o,M,M,M,M,o,o,o],
          [o,o,o,M,M,M,M,M,M,M,o],
          [o,o,o,M,M,o,M,M,M,o,o],
@@ -19,26 +18,24 @@ world = [[o,o,o,o,o,o,o,o,o,o,o],
          [o,o,o,o,o,o,o,o,o,o,o]]
 
 def continent_size world, x, y
-  if world[y][x] != 'land' # Why are x and y reversed? Does the order matter?
-  # Either it's water or we already counted it,
-  # but either way, we don't want to count it now.
+  if world[y][x] != 'land'  # y is presented first because of the visual layout of the array;
+                            # y represents the vertical axis 
     return 0
   end
-  #  So first we count this tile...
-  size = 1
+
+  size = 1                                          # Counts tile 5,5 as 1 (But isn't 5,5 water ("o")?)
   world[y][x] = 'counted land'
-  # ...then we count all of the neighboring eight tiles
-  # (and, of course, their neighbors by way of the recursion). size = size + continent_size(world, x-1, y-1)
-  size = size + continent_size(world, x , y-1)
-  size = size + continent_size(world, x+1, y-1)
-  size = size + continent_size(world, x-1, y )
-  size = size + continent_size(world, x+1, y )
-  size = size + continent_size(world, x-1, y+1)
-  size = size + continent_size(world, x , y+1)
-  size = size + continent_size(world, x+1, y+1)
+
+  size = size + continent_size(world, x-1, y-1)     # Checks 4,4 and checks everything connected
+  size = size + continent_size(world, x  , y-1)     # Checks 5,4 and checks everything connected
+  size = size + continent_size(world, x+1, y-1)     # Checks 6,4 and checks everything connected
+  size = size + continent_size(world, x-1, y  )     # Checks 4,5 and checks everything connected
+  size = size + continent_size(world, x+1, y  )     # Checks 6,5 and checks everything connected
+  size = size + continent_size(world, x-1, y+1)     # Checks 4,6 and checks everything connected
+  size = size + continent_size(world, x  , y+1)     # Checks 5,6 and checks everything connected
+  size = size + continent_size(world, x+1, y+1)     # Checks 6,6 and checks everything connected
   size
 end
 
 puts continent_size(world, 5, 5)
-# ?: I cut and pasted the code as is and got 21 as the output...
-# ?: How are tiles being marked as visited?
+
